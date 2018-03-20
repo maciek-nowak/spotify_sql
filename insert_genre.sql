17ce98a2a8d670ed252770d3c3e84f139731b53c
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-03-20 14:59:17 +0100

    Add insert_genre.sql

---
+++ insert_genre.sql
@@ -0,0 +1,10 @@
+insert into genre (id, name) values (1, 'Blues rock');
+insert into genre (id, name) values (2, 'Classic country');
+insert into genre (id, name) values (3, 'Electro music');
+insert into genre (id, name) values (4, 'Celtic music');
+insert into genre (id, name) values (5, 'Hip hop');
+insert into genre (id, name) values (6, 'Smooth jazz');
+insert into genre (id, name) values (7, 'Britpop');
+insert into genre (id, name) values (8, 'Hard rock');
+insert into genre (id, name) values (9, 'Folk metal');
+insert into genre (id, name) values (10, 'Punk rock');
